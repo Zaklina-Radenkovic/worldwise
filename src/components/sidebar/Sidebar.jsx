f0cba62e535064495dfa8e1f5d19b01a805b8ea6
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-02-09 20:40:07 +0100

    adding pages and components wit their styles

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import AppNav from "../appNav/AppNav";
+import Footer from "../footer/Footer";
+import Logo from "../logo/Logo";
+import styles from "./Sidebar.module.css";
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,12 @@
-import AppNav from "../components/AppNav";
+import Map from "../components/map/Map";
+import Sidebar from "../components/sidebar/Sidebar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }

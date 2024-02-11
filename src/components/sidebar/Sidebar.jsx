fba9792a33160ad98063abb02a84fc80936308ff
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-02-11 17:09:51 +0100

    adding child routes and index route

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import AppNav from "../appNav/AppNav";
 import Footer from "../footer/Footer";
 import Logo from "../logo/Logo";
@@ -8,7 +9,7 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-
+      <Outlet />
       <p>List of cities</p>
       <Footer />
     </div>

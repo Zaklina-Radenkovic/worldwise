a67cae41e344a85a22f12826bbf9e794907b60a9
Zaklina Radenkovic <zradenkovic@hotmail.com>
2024-02-17 16:50:02 +0100

    developing Map component

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,24 +1,93 @@
+import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import { useCities } from "../../context/CitiesContext";
+import { flagemojiToPNG } from "../../utils/flagemojiToPNG";
 import styles from "./Map.module.css";
+import { useGeolocation } from "../../hooks/useGeolocation";
+import Button from "../button/Button";
+import { useUrlPosition } from "../../hooks/useUrlPosition";
 
 function Map() {
-  const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
+  const [mapLat, mapLng] = useUrlPosition();
+
+  //in order for MAP to remember the last lat and lng even when we go back in another component, we need to synchronize 'mapLat' and 'mapLng' with 'mapPosition'
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
+
+  //synchronizing our geoLocation with mapposition
+  useEffect(() => {
+    if (geolocationPosition)
+      setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+  }, [geolocationPosition]);
 
   return (
-    <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {lat} {lng}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 23, lng: 48 })}>
-        Change pos
-      </button>
+    <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getPosition}>
+          {isLoadingPosition ? "Loading..." : "Use your position"}
+        </Button>
+      )}
+      <MapContainer
+        center={mapPosition}
+        zoom={6}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{flagemojiToPNG(city.emoji)}</span>{" "}
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
+      </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+  useMapEvents({
+    click: (e) => {
+      const { lat, lng } = e.latlng;
+      //when we have multiple clicks on MAP, to go back to the last visited route
+      navigate(`form?lat=${lat}&lng=${lng}`, { replace: true });
+    },
+  });
+  return null;
 }
 
 export default Map;
